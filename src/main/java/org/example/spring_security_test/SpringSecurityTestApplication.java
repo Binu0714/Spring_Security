a0a25183185dca664e68f799ed5b16f03573a2f2
Binu0714 <binuj714@gmail.com>
2025-08-05 22:48:29 +0530

    create project structure

--- src/main/java/org/example/spring_security_test/SpringSecurityTestApplication.java
+++ src/main/java/org/example/spring_security_test/SpringSecurityTestApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringSecurityTestApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringSecurityTestApplication.class, args);
 	}
 
